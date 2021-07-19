6b9086c60f4ac74da2feae56f2cd96f2424fca69
Nicolas Minoli <minoli.nicolas@gmail.com>
2021-07-18 22:52:59 -0300

    Adding findAll functionality endpoint

--- src/main/java/com/product/productManager/controller/ProductController.java
+++ src/main/java/com/product/productManager/controller/ProductController.java
@@ -2,14 +2,10 @@ package com.product.productManager.controller;
 
 import com.product.productManager.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.product.productManager.domain.model.Product;
+
+import java.util.List;
 
 
 @RestController
@@ -24,16 +20,22 @@ public class ProductController {
         return productService.getProduct(id);
     }
 
+    @GetMapping( produces = "application/json")
+    public List<Product> getProduct(){
+        return productService.getAll();
+    }
+
     @PostMapping(produces = "application/json")
     public Product newProduct(@RequestBody Product newProduct) {
         return productService.newProduct(newProduct);
     }
 
-    @DeleteMapping(produces = "application/json")
+    @DeleteMapping(value = "/{id}", produces = "application/json")
     public void removeProduct(@PathVariable int id) {
         productService.deleteProduct(id);
     }
 
+    @PutMapping(value = "/{id}", produces = "application/json")
     public Product updateProduct(@PathVariable int id, @RequestBody Product updateProduct) {
         return productService.updateProduct(id, updateProduct);
     }
